2d866d50d3640f013d92fd042d021c5dac0ee5ec
Tom Brzozowski <tom@dfxtraders.com>
2025-01-26 21:13:03 +0000

    version bump

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@
 set -e
 
 cargo publish -p metricus
-cargo publish -p metricus_agent
-cargo publish -p metricus_allocator
 cargo publish -p metricus_macros
+cargo publish -p metricus_allocator
+cargo publish -p metricus_agent
